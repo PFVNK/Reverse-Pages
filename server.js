0c512f8fd9b60b90705e897ba7fb384ecc508c22
Patrick Heflin <patrickslx@yahoo.com>
2020-12-08 14:08:48 -0600

    *

--- server.js
+++ server.js
@@ -9,5 +9,11 @@ const app = express()
 app.use(morgan('tiny'))
 app.use(cors())
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
 const port = process.env.PORT || 3001
 app.listen(port, () => console.log(`app listening on ${port}`))
